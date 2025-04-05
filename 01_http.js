045c52c9bf8532392c5cc05a2426742408e06197
TelTrekker <magpayok707@gmail.com>
2025-04-05 13:37:38 +0800

    done at midterm-exam

--- 01_http.js
+++ 01_http.js
@@ -2,28 +2,20 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        // res.statusCode = 200;
-        // res.setHeader('Content-Type': 'application/json');
-        res.writehead(200, {'Content-Type':'application/json'});
+        const date = new Date();
+        res.writeHead(200, {'Content-Type':'application/json'});
         res.end(JSON.stringify({
-            date : newDate().toISOString()
+            date : date.toISOString()
         }));
     } else if (req.url === '/hello') {
-
-        res.writehead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end("Hello, World!");
     } else {
-        res.writehead(404, {'Content-Type': 'text/plain'});
+        res.writeead(404, {'Content-Type': 'text/plain'});
         res.end('Page not found');
     }
 });
 
-// const port = 3000;
-
-// server.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}/`);
-// });
-
-server.listen(3000, () => {
+server.listen(3000, 'localhost', () => {
     console.log('Server running at http://localhost:3000/')
 });
